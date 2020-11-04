33c89de1139133a2f426f3be0b987d324a913808
Kerkeb Abdou <Kerkeb@gmail.com>
2020-11-04 16:24:58 +0100

    add props

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+import './Header.css'
+
+class Header extends React.Component {
+	
+	render(){
+		console.log('this : ',this.props)
+		return (<header className="header-header">
+			<img src={logo} className="header-logo" alt="logo" />
+			<p>
+				j'ai {this.props.age} ans mon status {this.props.status}
+			</p>
+
+		</header>
+		)
+	}
+}
+
+// const Header = ({age, status}) => {
+// 	// console.log(props)
+// 	// const {age, status} = props
+//   return (
+// 		<header className="header-header">
+// 			<img src={logo} className="header-logo" alt="logo" />
+// 			<p>
+// 				j'ai {age} ans mon status {status}
+// 			</p>
+
+// 		</header>
+// 	)
+// }
+
+export default Header
